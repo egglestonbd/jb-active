effdeec4f6b25bd2dea4946b352d977e8fd49b66
Brent D Eggleston <EgglestonBD@familysearch.org>
2019-01-01 14:45:16 -0700

    Add initial Application
    Application Launches with
    `java -cp target/jb-active-1.0-SNAPSHOT.jar org.jbactive.Work`

---
+++ src/main/java/org/jbactive/Work.java
@@ -0,0 +1,14 @@
+/*
+ * © 2018 by Intellectual Reserve, Inc. All rights reserved.
+ */
+
+package org.jbactive;
+
+public class Work {
+
+  public static void main(String[] args) {
+    System.out.println("Launching.");
+
+  }
+
+}
