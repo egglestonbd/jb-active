a917a1667d9d0d55f6aaaf126d5a5c09613f033f
Brent D Eggleston <EgglestonBD@familysearch.org>
2019-01-01 15:07:38 -0700

    Get Logging

--- src/main/java/org/jbactive/Work.java
+++ src/main/java/org/jbactive/Work.java
@@ -4,10 +4,17 @@
 
 package org.jbactive;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Work {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(Work.class);
+
+
   public static void main(String[] args) {
-    System.out.println("Launching.");
+
+    LOGGER.info("Launching Application.");
 
   }
 
